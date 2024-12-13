bdebed6373a257cad708d4b309b979e4ca1fc7ee
Anurag <149858984+Yeerapanenianurag@users.noreply.github.com>
2024-12-13 16:29:06 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,20 @@
+# write a program to identify the all datatype
+# write a program to print in 2 lines
+
+#printing different datatypes
+a = 1
+b= "Hello world"
+c = 0.445
+d = True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+
+#printing names in 2 different lines 
+print("hello\nworld")
+
+#printing 2 different print statements in a single line 
+print ("hello",end = " ")
+print("people")
