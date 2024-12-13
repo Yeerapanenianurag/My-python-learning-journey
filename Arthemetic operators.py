c581d9a6e613e19367bf648e67ff532bb2d75a8d
Anurag <149858984+Yeerapanenianurag@users.noreply.github.com>
2024-12-13 16:29:28 +0530

    Add files via upload

---
+++ Arthemetic operators.py
@@ -0,0 +1,11 @@
+#arthemetic operators
+num1 = 9 
+num2 = 6
+
+addition = print("the sum is :",num1 + num2)
+subtraction = print("the difference is :",num1 - num2)
+multiplication = print("the product is :" , num1 * num2)
+division = print("the divison is : " ,num1 / num2)
+power = print("the power is :",num1 ** num2)
+double_division = print("the double division is :",num1 // num2)
+remainder = print("the reminder is ",num1 % num2)
